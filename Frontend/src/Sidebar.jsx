2e7f19aeb86c75d71f7fda87fb781496a2feaa49
HackerPrince4321 <princesoni8761969@gmail.com>
2025-10-05 21:31:58 +0530

    Update Sidebar.jsx

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -21,7 +21,7 @@ function Sidebar({ isOpen, onClose, onLogout }) {
           <button onClick={onClose} className="close-btn">&times;</button>
         </div>
         <ul className="sidebar-menu" style={{maxHeight: 'auto', overflowY: 'auto'}}>
-          <li onClick={() => handleOptionClick('Change Name')}>Change Name</li>
+          {/* <li onClick={() => handleOptionClick('Change Name')}>Change Name</li>
           <li onClick={() => handleOptionClick('Change Password')}>Change Password</li>
           <li onClick={() => handleOptionClick('Add Email')}>Add Email</li>
           <li onClick={() => handleOptionClick('Add Profile Pic')}>Add Profile Pic</li>
@@ -29,7 +29,7 @@ function Sidebar({ isOpen, onClose, onLogout }) {
           <li onClick={() => handleOptionClick('Feedback')}>Feedback</li>
           <li onClick={() => handleOptionClick('About')}>About</li>
           <li onClick={() => handleOptionClick('Certification')}>Certification</li>
-          <li onClick={() => handleOptionClick('Quiz History')}>Quiz History</li>
+          <li onClick={() => handleOptionClick('Quiz History')}>Quiz History</li> */}
           <li onClick={onLogout}>Logout</li>
         </ul>
       </div>
