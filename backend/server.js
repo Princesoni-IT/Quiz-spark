b2c39a408c4ad4ea4e7c55685a99d1c9098fbacd
HackerPrince4321 <princesoni8761969@gmail.com>
2025-10-06 08:54:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "http://localhost:5173", methods: ["GET", "POST", "PUT", "DELETE"] }});
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
