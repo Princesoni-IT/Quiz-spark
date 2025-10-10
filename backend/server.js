12cc48621cf8bb4a923b64b1dcdfc5600b07895d
HackerPrince4321 <princesoni8761969@gmail.com>
2025-10-10 19:42:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: ["http://localhost:5173", "https://spark-quiz.netlify.app/"], methods: ["GET", "POST", "PUT", "DELETE"] }});
+const io = new Server(server, { cors: { origin: ["http://localhost:5173", "https://quiz-spark.netlify.app"], methods: ["GET", "POST", "PUT", "DELETE"] }});
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
