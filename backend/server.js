5220c9abf25a22afadf73f7bfbc3ccee747e6a38
HackerPrince4321 <princesoni8761969@gmail.com>
2025-10-12 09:54:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -177,7 +177,13 @@ app.post('/api/register', async (req, res) => {
 
 
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                
+
+                    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+
+
                 auth: {
                     user: process.env.GMAIL_USER,
                     pass: process.env.GMAIL_PASS
